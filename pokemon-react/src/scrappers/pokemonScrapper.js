6d9a3d2f1c946d0062030e42e3727b8fb1090ffb
Alejandro C. Grant <authorcgrant@gmail.com>
2022-02-28 13:58:44 -0800

    scraper pipeline established

--- pokemon-react/src/scrappers/pokemonScrapper.js
+++ pokemon-react/src/scrappers/pokemonScrapper.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const PokemonScrapper = async () => {
   const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151%27');
   const pokemon = res.data.results;
+  const pokemonArr = []
 
   pokemon.forEach(async (poke) => {
     const url = poke.url;
@@ -11,6 +12,11 @@ const PokemonScrapper = async () => {
     const abilities = pokemonData.data.abilities;
     const typesArr = pokemonData.data.types.map((ele) => ele.type.name);
     console.log(typesArr)
+  })
+
+  fs.writeFile('../../../pokemon-flask/seeds/pokemon_seeds.json', JSON.stringify(pokemonArr), (err) => {
+    if (err) console.error(`A wild error has appeared in the bushes: ${err}`);
+    else console.log(`Successful scrape! Count: ${pokemonArr.length}`)
   })
 }
 PokemonScrapper()
