30837a8ecbb17a71cc5c0cf068f004181ba721a2
Alejandro C. Grant <authorcgrant@gmail.com>
2022-02-28 13:39:49 -0800

    targeting pokemon data

--- pokemon-react/src/scrappers/pokemonScrapper.js
+++ pokemon-react/src/scrappers/pokemonScrapper.js
@@ -1,10 +1,16 @@
-import axios from 'axios'
-import cheerio from 'cheerio'
+const axios = require('axios');
+const fs = require('fs');
 
 const PokemonScrapper = async () => {
-   let res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151%27')
-   console.log(res)
+  const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151%27');
+  const pokemon = res.data.results;
+
+  pokemon.forEach(async (poke) => {
+    const url = poke.url;
+    const pokemonData = await axios.get(url);
+    const abilities = pokemonData.data.abilities;
+    const typesArr = pokemonData.data.types.map((ele) => ele.type.name);
+    console.log(typesArr)
+  })
 }
 PokemonScrapper()
-
-export default PokemonScrapper;
