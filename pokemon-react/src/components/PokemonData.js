1e057ebace05111745b96f727bf08ac148b5720a
Alex Monson <monson.alex@rocketmail.com>
2022-02-28 12:26:59 -0800

    got images set up

--- pokemon-react/src/components/PokemonData.js
+++ pokemon-react/src/components/PokemonData.js
@@ -24,11 +24,14 @@ useEffect(() => {
 return(
     <div>
         <ul>
-            {data.results && data.results.map(pokemon => <li key={pokemon.name}>{pokemon.name}</li>)}
+            {data.results && data.results.map((pokemon, i) => {
+                let pokeUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png`;
+                return <li key={pokemon.name}> {pokemon.name}
+                <img src={pokeUrl} alt=''/> 
+                </li>
+             })}
         </ul>
     </div>
-
-    
     )
 }
 
