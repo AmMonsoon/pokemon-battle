1871af6157178e8708d88c788dd64d98d2cd1a7e
Alejandro C. Grant <authorcgrant@gmail.com>
2022-03-05 15:01:08 -0800

    just made the abilities model

---
+++ pokemon-flask/models/abilities.py
@@ -0,0 +1,12 @@
+"""Abilities model."""
+
+from .db import db
+
+class Abilities(db.Model):
+    """Abilities model class."""
+
+    __tablename__ = 'abilities'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    damage = db.Column(db.SmallInteger, nullable=True)
--- pokemon-flask/models/pokemon.py
+++ pokemon-flask/models/pokemon.py
@@ -8,6 +8,7 @@ class Pokemon(db.Model):
     __tablename__ = 'pokemon'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
     avatar = db.Column(db.String(1000), nullable=True)
     ability_1 = db.Column(db.String(30), nullable=False)
     ability_2 = db.Column(db.String(30), nullable=False)
